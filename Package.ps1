008807dbf77b6c7f7ed2ebec50853364d26df82e
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-04-06 19:10:34 -0400

    Package .zip change

--- Package.ps1
+++ Package.ps1
@@ -2,15 +2,16 @@
 
 $ModName = "Guil-晓月食物";
 $ModFolder = "./Package/" + $ModName
+$ArchiveName = "Guil-MoonFood.zip"
 
 mkdir -ErrorAction SilentlyContinue $ModFolder
 Remove-Item -ErrorAction SilentlyContinue -Recurse ./Package/*
-Remove-Item -ErrorAction SilentlyContinue "$ModName.zip"
+Remove-Item -ErrorAction SilentlyContinue $ArchiveName
 
 dotnet publish .\src\晓月食物.csproj -o $ModFolder
 
 Copy-Item -Recurse ./Guil-晓月食物/* $ModFolder
 
 # English name since github strips Unicode for security purposes.
-Compress-Archive -DestinationPath "Guil-MoonFood.zip" -Path ./Package/*
+Compress-Archive -DestinationPath $ArchiveName -Path ./Package/*
 
