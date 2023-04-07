635756d9854e370e9cc9572a898cd9bfbd421dec
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2023-04-07 13:33:31 -0400

    Removed unused references

--- src/LocalizationStringUtility.cs
+++ src/LocalizationStringUtility.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
-using BepInEx;
 using BepInEx.Logging;
-using HarmonyLib;
-using UnityEngine;
-using 饥荒食物;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace LocalizationUtilities
 {
